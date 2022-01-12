ae94ff64fa054031d0fa57a797ab651efc7babf3
himchan94 <himchan9494@gmail.com>
2022-01-12 20:32:31 +0900

    redux-thunk

---
+++ redux-thunk/src/containers/UserListContainer.jsx
@@ -0,0 +1,33 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import UserList from "../components/UserList";
+import { getUsersThunk } from "../redux/actions";
+
+const UserListContainer = () => {
+  const users = useSelector((state) => state.users.data);
+  const dispatch = useDispatch();
+
+  // const getUsers = useCallback(async () => {
+  //   try {
+  //     dispatch(getUsersStart());
+  //     const res = await axios.get("https://api.github.com/users");
+  //     dispatch(getUsersSuccess(res.data));
+  //   } catch (error) {
+  //     dispatch(getUsersFail(error));
+  //   }
+  // }, [dispatch]);
+
+  const getUsers = useCallback(() => {
+    dispatch(getUsersThunk());
+  }, [dispatch]);
+
+  const props = {
+    users,
+    getUsers,
+  };
+
+  return <UserList {...props} />;
+};
+
+export default UserListContainer;
