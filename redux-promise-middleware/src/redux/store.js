763a495a777a6571addf61a060f6e8a1efa60896
himchan94 <himchan9494@gmail.com>
2022-01-12 20:29:40 +0900

    redux-promise-middleware

---
+++ redux-promise-middleware/src/redux/store.js
@@ -0,0 +1,41 @@
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./reducers/reducers.js";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
+
+// middleware
+
+// const middleware1 = (store) => {
+//   console.log("middleware1", 0);
+//   return (next) => {
+//     console.log("middleware1", 1, next);
+//     return (action) => {
+//       console.log("middleware1", 2, action);
+//       const returnValue = next(action);
+//       console.log("middleware1", 3);
+//       return returnValue;
+//     };
+//   };
+// };
+
+// const middleware2 = (store) => {
+//   console.log("middleware2", 0);
+//   return (next) => {
+//     console.log("middleware2", 1, next);
+//     return (action) => {
+//       console.log("middleware2", 2, action);
+//       const returnValue = next(action);
+//       console.log("middleware2", 3);
+//       return returnValue;
+//     };
+//   };
+// };
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, promise))
+);
+// 액션생성자가 함수를 반환할 때만 동작함
+// 일반 객체가 반환되면 thunk가 반응하지 않음
+export default store;
