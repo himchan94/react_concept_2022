f7f1ddbd42b54b3482b694ae4a84250981f66873
himchan94 <himchan9494@gmail.com>
2022-01-17 16:34:42 +0900

    thunk model

---
+++ redux-thunk-dataModel/src/actions.js
@@ -0,0 +1,42 @@
+import Axios from "axios";
+
+// export const fetchPosts = async () => {
+//   const response = await Axios.get(
+//     "https://jsonplaceholder.typicode.com/posts"
+//   );
+
+//   return {
+//     type: "FETCH_POSTS",
+//     payload: response.data,
+//   };
+// };
+
+// export const fetchPosts = () => {
+//   const promise = Axios.get("https://jsonplaceholder.typicode.com/posts");
+
+//   return {
+//     type: "FETCH_POSTS",
+//     payload: promise,
+//   };
+// };
+
+// export const fetchPosts = () => async (dispatch, getState) => {
+//   const response = await Axios.get(
+//     "https://jsonplaceholder.typicode.com/posts"
+//   );
+//   console.log(response.data);
+//   dispatch({ type: "FETCH_POSTS", payload: response.data });
+// };
+
+export const fetchPosts = () => async (dispatch, getState) => {
+  dispatch({ type: "FETCH_POSTS_REQUEST" });
+  try {
+    const response = await Axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    dispatch({ type: "FETCH_POSTS_SUCCESS", payload: response.data });
+  } catch (error) {
+    dispatch({ type: "FETCH_POSTS_FAILURE", error });
+  }
+};
---
+++ redux-thunk-dataModel/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import App from "./App.jsx";
+import postReducer from "./reducer.js";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+  postReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
